c65792c18e686b776e44b40a39ae6e3756a22a45
Yannis <sebastienyannis@gmail.com>
2026-07-20 17:50:05 +0300

    load program into memory

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use crate::cpu::CPU;
+
+pub mod cpu;
+
 fn main() {
-    println!("Hello, world!");
+    let mut cpu = CPU::new();
+    let is_load_ok =cpu.load_program("C:/Users/sebas/Documents/dev/cheap-8/IBM Logo.ch8");
+
+    match is_load_ok {
+        Ok(_) => println!("Program loaded into memory"),
+        Err(_) => println!("Couldn't load the program into memory")
+    }
 }
